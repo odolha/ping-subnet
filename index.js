8c42dde69ee1f0edafc2480aea70a7e906d99a94
Dmitrii Bondarchuk <dmitrii.bondarchuk@iii.com>
2017-09-25 16:59:23 +0300

    Provided function for subnets ranges getting

---
+++ index.js
@@ -0,0 +1,35 @@
+const os = require('os');
+const ipUtils = require('./ipUtils');
+
+const findSubnetsRange = () => {
+  const networkInterfaces = os.networkInterfaces();
+
+  return Object.keys(networkInterfaces)
+    .filter(key => !networkInterfaces[key][0].internal)
+    .map(key => {
+      const {address, netmask} = networkInterfaces[key][1];
+      const addressNumber = ipUtils.ip2number(address) >>> 0;
+      const netmaskNumber = ipUtils.ip2number(netmask) >>> 0;
+      const baseAddress = ipUtils.number2ip(addressNumber & netmaskNumber);
+      let bitMask;
+
+      for(let i = 32; i >= 0; i--) {
+        if(netmaskNumber == (0xffffffff << (32 - i)) >>> 0) {
+          bitMask = i;
+        }
+      }
+
+      return {
+        address, 
+        netmask,
+        leftBound: bitMask <= 30
+          ? ipUtils.number2ip((addressNumber & netmaskNumber) + 1) 
+          : baseAddress,
+        rightBound: bitMask <= 30
+          ? ipUtils.number2ip((addressNumber & netmaskNumber) + Math.pow(2, 32 - bitMask) - 2)
+          : ipUtils.number2ip((addressNumber & netmaskNumber) + Math.pow(2, 32 - bitMask) - 1)
+      };
+    });
+}
+
+module.exports = {findSubnetsRange};
