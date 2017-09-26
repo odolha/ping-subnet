f8e84251ffe66a2eaffa96443a5c1d3105d04392
dvbondarchuk@gmail.com <dvbondarchuk@gmail.com>
2017-09-26 22:31:07 +0300

    Changed example

--- example.js
+++ example.js
@@ -1,8 +1,13 @@
 const SubnetsPinger = require('./');
 
 const subnetPinge = new SubnetsPinger();
+
 subnetPinge.on('host:alive', ip => {
   console.log('alive', ip);
+});
+
+subnetPinge.once('ping:end', () => {
+  console.log('ping completed');
 });
 
 subnetPinge.ping();
