5ebfa7c80a739aeee6d994c9b77b315a1b8cabe9
Dmitrii Bondarchuk <dmitrii.bondarchuk@iii.com>
2017-09-26 15:05:31 +0300

    Rewrited as the class extending EventEmmiter

--- index.js
+++ index.js
@@ -1,51 +1,73 @@
 const os = require('os');
+const EventEmitter = require('events');
 
 const ping = require('ping');
 
 const ipUtils = require('./ipUtils');
 
-const findSubnetsRange = () => {
-  const networkInterfaces = os.networkInterfaces();
+class SubnetsPinger extends EventEmitter {
+  constructor() {
+    super();
 
-  return Object.keys(networkInterfaces)
-    .filter(key => !networkInterfaces[key][0].internal)
-    .map(key => {
-      const { address, netmask } = networkInterfaces[key][1];
-      const addressNumber = ipUtils.ip2number(address) >>> 0;
-      const netmaskNumber = ipUtils.ip2number(netmask) >>> 0;
-      const baseAddress = ipUtils.number2ip(addressNumber & netmaskNumber);
-      let bitMask;
+    const networkInterfaces = os.networkInterfaces();
+    this.ranges = Object.keys(networkInterfaces)
+      .filter(key => !networkInterfaces[key][0].internal)
+      .map(key => {
+        const { address, netmask } = networkInterfaces[key][1];
+        const addressNumber = ipUtils.ip2number(address) >>> 0;
+        const netmaskNumber = ipUtils.ip2number(netmask) >>> 0;
+        const baseAddress = ipUtils.number2ip(addressNumber & netmaskNumber);
+        let bitMask;
 
-      for (let i = 32; i >= 0; i--) {
-        if (netmaskNumber == (0xffffffff << (32 - i)) >>> 0) {
-          bitMask = i;
+        for (let i = 32; i >= 0; i--) {
+          if (netmaskNumber == (0xffffffff << (32 - i)) >>> 0) {
+            bitMask = i;
+          }
         }
+
+        return {
+          leftBound: bitMask <= 30
+            ? ipUtils.number2ip((addressNumber & netmaskNumber) + 1)
+            : baseAddress,
+          rightBound: bitMask <= 30
+            ? ipUtils.number2ip((addressNumber & netmaskNumber) + Math.pow(2, 32 - bitMask) - 2)
+            : ipUtils.number2ip((addressNumber & netmaskNumber) + Math.pow(2, 32 - bitMask) - 1)
+        };
+      });
+  }
+
+  ping() {
+    const ips = this.ips;
+    const loop = () => {
+      let ip;
+      if(ip = ips.pop()) {
+        ping.promise.probe(ip).then(target => {
+          if(target.alive) {
+            this.emit('host:alive', target.host);
+          }
+          loop();
+        });
       }
-
-      return {
-        leftBound: bitMask <= 30
-          ? ipUtils.number2ip((addressNumber & netmaskNumber) + 1)
-          : baseAddress,
-        rightBound: bitMask <= 30
-          ? ipUtils.number2ip((addressNumber & netmaskNumber) + Math.pow(2, 32 - bitMask) - 2)
-          : ipUtils.number2ip((addressNumber & netmaskNumber) + Math.pow(2, 32 - bitMask) - 1)
-      };
-    });
-};
-
-const pingSubnets = () => {
-  const ranges = findSubnetsRange();
-  const ips = [];
-  for (let i = 0; i < ranges.length; i++) {
-    const startRange = ipUtils.ip2number(ranges[i].leftBound);
-    const endRange = ipUtils.ip2number(ranges[i].rightBound)
-    for (let j = startRange; j <= endRange; j++) {
-      ips.push(ipUtils.number2ip(j));
+    };
+    for(let i = 0; i < 50; i++) {
+      process.nextTick(() => {
+        loop();
+      });
     }
   }
 
-  return Promise.all(ips.map(ip => ping.promise.probe(ip)))
-    .then(ips => ips.filter(ip => ip.alive).map(ip => ip.host));
-};
+  get ips() {
+    const ips = [];
+    for (let i = 0; i < this.ranges.length; i++) {
+      const startRange = ipUtils.ip2number(this.ranges[i].leftBound);
+      const endRange = ipUtils.ip2number(this.ranges[i].rightBound);
+      for (let j = startRange; j <= endRange; j++) {
+        ips.push(ipUtils.number2ip(j));
+      }
+    }
 
-module.exports = { findSubnetsRange, pingSubnets };
+    return ips;
+  }
+}
+
+module.exports = SubnetsPinger;
